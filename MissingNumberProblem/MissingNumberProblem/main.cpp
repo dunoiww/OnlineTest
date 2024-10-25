ae6525e7100ae54350495335b650ea67b00a02cf
Ngo Nam <namngo102003@gmail.com>
2024-10-25 11:32:00 +0700

    Question 2.2

---
+++ MissingNumberProblem/MissingNumberProblem/main.cpp
@@ -0,0 +1,32 @@
+//
+//  main.cpp
+//  MissingNumberProblem
+//
+//  Created by Ngô Nam on 25/10/2024.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int missingNumber(int a[], int n) {
+    int sumOfN = (n + 1) * (n + 2) / 2;
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += a[i];
+    }
+    return sumOfN - sum;
+}
+
+int main(int argc, const char * argv[]) {
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+   
+    cout << missingNumber(a, n) << endl;
+    
+    return 0;
+}
